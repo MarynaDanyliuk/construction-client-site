723f328cba247d14d406c0838c7847bce76b05ec
Mary Danyliuk <marydanyliuk@gmail.com>
2025-06-22 17:53:06 +0300

    add middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -4,9 +4,28 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // Якщо на корені
+  // Якщо користувач на головній сторінці, редиректимо на /ua
   if (pathname === "/") {
-    const locale = "ua"; // тут можна динамічно визначати за мовою браузера
-    return NextResponse.redirect(new URL(`/${locale}`, request.url));
+    return NextResponse.redirect(new URL("/ua", request.url));
   }
+
+  // В інших випадках нічого не змінюємо
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/"], // middleware спрацьовує лише на /
+};
+
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+
+//   // Якщо на корені
+//   if (pathname === "/") {
+//     const locale = "ua"; // тут можна динамічно визначати за мовою браузера
+//     return NextResponse.redirect(new URL(`/${locale}`, request.url));
+//   }
+// }
